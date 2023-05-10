dd02626a7299b660d22f9e43026cbd1cc46b28d5
YURIRI <101607533+ZANGETU@users.noreply.github.com>
2023-05-10 17:43:53 +0900

    Update Form1.cs

--- YoutubeThumbnailDownloader/Form1.cs
+++ YoutubeThumbnailDownloader/Form1.cs
@@ -1,18 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Xml.Linq;
-using Google.Apis.Services;
-using Google.Apis.YouTube.v3;
-
 namespace YouTube_thumbnail_Save
 {
     public partial class Form1 : Form
