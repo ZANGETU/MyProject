69d7db33b57b17d2a5bd5e4507f8e96ee72ca1b8
YURIRI <101607533+ZANGETU@users.noreply.github.com>
2024-12-10 11:22:23 +0900

    Update Form1.cs

--- MinecraftPotionEditor/Form1.cs
+++ MinecraftPotionEditor/Form1.cs
@@ -1,15 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using MetroFramework.Forms;
-using DiscordRpcDemo;
-
 namespace Minecraft_Potion_Editor
 {
     public partial class Form1 : MetroForm
