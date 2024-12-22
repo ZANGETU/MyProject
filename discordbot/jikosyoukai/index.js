60073dc72c5c4b7a5eca6f19af943cd20baca0da
YURIRI <101607533+ZANGETU@users.noreply.github.com>
2024-12-22 20:04:29 +0900

    Update index.js

--- discordbot/jikosyoukai/index.js
+++ discordbot/jikosyoukai/index.js
@@ -25,11 +25,8 @@ const commands = [
 const rest = new REST({ version: '9' }).setToken(token);
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
-
         await rest.put(Routes.applicationCommands(clientId), { body: commands });
-
-        console.log('Successfully reloaded application (/) commands.');
+        console.log('起動に成功しました');
     } catch (error) {
         console.error(error);
     }
