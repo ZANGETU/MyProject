5436184863d0d03e7b6525bfbb8cf43c22f37ec4
YURIRI <101607533+ZANGETU@users.noreply.github.com>
2024-12-22 17:33:32 +0900

    Add files via upload

---
+++ GetISS/index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const ISS_API_URL = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+//ISSの現在位置
+async function getISS(){
+    try {
+        const response = await axios.get(ISS_API_URL);
+        const { latitude: ido, longitude: keido, altitude: kodo, velocity: sokudo, visibility: kansoku } = response.data;        
+        console.log('ISS現在位置:');
+        console.log(`緯度: ${ido.toFixed(2)}`);
+        console.log(`経度: ${keido.toFixed(2)}`);
+        console.log(`高度: ${kodo.toFixed(2)} km`);
+        console.log(`速度: ${sokudo.toFixed(2)} km/h`);
+        console.log(`観測の可能性: ${kansoku === 'eclipsed' ? '見えねーよ' : '見えるぞ'}`);
+    } catch (error) {
+        console.error('データの取得エラー', error.message);
+    }
+}
+getISS();
